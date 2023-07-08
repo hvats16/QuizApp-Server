8780c1adda6a79b480d5bb745dbea0d7b0952928
Harshit Vats <hvats16@gmail.com>
2023-07-09 01:04:05 +0530

    Backend Completed

--- models/questionSchema.js
+++ models/questionSchema.js
@@ -3,7 +3,7 @@ const { Schema } = mongoose;
 
 /** question model */
 const questionModel = new Schema({
-	questions: { type: Array, default: [] },
+	questions: { type: Array, default: [] }, // create question with [] default value
 	answers: { type: Array, default: [] },
 	createdAt: { type: Date, default: Date.now },
 });
