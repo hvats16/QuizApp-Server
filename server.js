f7a9cba1141a3baa244985d4894d6cd014bf63d0
Harshit Vats <hvats16@gmail.com>
2023-07-08 23:56:44 +0530

    Starter File

---
+++ server.js
@@ -0,0 +1,28 @@
+import cors from "cors";
+import { config } from "dotenv";
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+
+/** app middlewares */
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+config();
+
+/** appliation port */
+const port = process.env.PORT || 8080;
+
+/** routes */
+app.get("/", (req, res) => {
+	try {
+		res.json("Get Request");
+	} catch (error) {
+		res.json(error);
+	}
+});
+
+app.listen(port, () => {
+	console.log(`Server connected to http://localhost:${port}`);
+});
